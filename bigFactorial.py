345b3ecd7831e58d9b3908eb6df58aedeff7da84
Dev-Moynul <stu.moynulislam@gmail.com>
2025-12-10 15:58:05 +0600

    big factorial

--- bigFactorial.py
+++ bigFactorial.py
@@ -1,10 +1,10 @@
-N = int(input())
+n = int(input())
 
 # For large N >= 25, factorial ends with at least four zeros
-if N >= 25:
+if n >= 25:
     print(0)
 else:
     fact = 1
-    for i in range(1, N + 1):
+    for i in range(1, n + 1):
         fact = (fact * i) % 10000
     print(fact)
